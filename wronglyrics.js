963bf1e51f3df260ddebb60340209a3097863f68
Mark Edelman <markedelman1987@gmail.com>
2016-12-08 16:07:50 -0500

    get json to ajax switch with alex

--- wronglyrics.js
+++ wronglyrics.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 var musicMatchApiUrl = 'http://api.musixmatch.com/ws/1.1/'
-
-function getMusicMatchAPI(userQuery, callback){
-	var apiKey = '758a9c4eb110abe05628670f79cacb15';
-
-	var request = {
-		apikey: apiKey,
-		q: '' 
-	}
-
-	$.getJSON(musicMatchApiUrl, request, callback)
-
-}
-
-
+var objectKey = '758a9c4eb110abe05628670f79cacb15'
+$.ajax({
+    type: "GET",
+    data: {
+        apikey: objectKey,
+        q_artist: 'Disturbed',
+        format:"jsonp",
+        callback:"jsonp_callback"
+    },
+    url: "http://api.musixmatch.com/ws/1.1/track.search"
+  }).done(function(data){
+    console.log(data)
+  })
 
 
 
